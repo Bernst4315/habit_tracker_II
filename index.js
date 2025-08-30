b9e063206a3e482fc4bba4c00af77f11c2626112
bernst4315 <bernst4315@gmail.com>
2025-08-30 13:20:57 -0600

    Add habit button now functions.  habits array now renders in DOM  Fixed - habit-div  in html was made into an id better matching css file

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-console.log("hello world")
+//Variables
+
+const habitDiv = document.querySelector("#habit-div")
+const habitEl = document.querySelector("#habitInput-el")
+const addBtn = document.querySelector("#addHabit-btn")
+const habitList = document.querySelector("#habit-list")
+const habits = []
+console.log(habitList)
+
+//Event Listeners 
+
+addBtn.addEventListener("click", function(){
+   
+    let habit = "";
+    habit = habitEl.value
+    habits.push(habit)
+    console.log(habit)
+    habitEl.value = ""
+    console.log(habits)
+    addHabit()
+})
+
+//Functions
+
+function addHabit(){
+
+    //const liEl = document.createElement("li")
+    let str = ""
+    for(let i = 0; i < habits.length; i++){
+        str += `<li>${habits[i]}</li>`
+    }
+    habitList.innerHTML = str 
+}
