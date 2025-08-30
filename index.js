372727b83aef6d7e9fe52b96d098455765cd9aaf
bernst4315 <bernst4315@gmail.com>
2025-08-30 14:45:28 -0600

    added a div for each habit. so later it can be tracked

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const habitDiv = document.querySelector("#habit-div")
 const habitEl = document.querySelector("#habitInput-el")
 const addBtn = document.querySelector("#addHabit-btn")
 const habitList = document.querySelector("#habit-list")
+const habitItem = document.querySelector(".habit-item")
 const habits = []
 console.log(habitList)
 
@@ -28,7 +29,17 @@ function addHabit(){
     //const liEl = document.createElement("li")
     let str = ""
     for(let i = 0; i < habits.length; i++){ //find a way to update list w/out rerendering whole array
-        str += `<li>${habits[i]}</li>`
+        // str += `<li>${habits[i]}</li>`
+        str += `
+        <div class="border habit-item">
+            <h3>${habits[i]}</h3>
+                <div class="counter">
+                    <p>0</p>
+                    <button>+</button>
+                </div>
+        </div>
+        `
     }
-    habitList.innerHTML = str //This updates the DOM once. keep this in mind for efficiency 
+    //habitList.innerHTML = str //This updates the DOM once. keep this in mind for efficiency 
+    habitList.innerHTML = str
 }
