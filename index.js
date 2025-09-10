b46edc503d4f049d49b4a1f917cf502c5f78fd12
bernst4315 <bernst4315@gmail.com>
2025-09-10 13:17:03 -0600

    added increment functionallity

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
+import { v4 as uuidv4 } from 'https://cdn.jsdelivr.net/npm/uuid@13.0.0/+esm'
+
 //Variables
 
-const habitDiv = document.querySelector("#habit-div")
 const habitEl = document.querySelector("#habitInput-el")
-const addBtn = document.querySelector("#addHabit-btn")
 const habitList = document.querySelector("#habit-list")
-const habitItem = document.querySelector(".habit-item")
 const habits = []
-console.log(habitList)
 
 //Event Listeners 
 
@@ -14,10 +12,9 @@ document.addEventListener("click", function(e){
     
     switch(e.target.id){
         case "addHabit-btn":
-            console.log(e.target.id)
             addHabit()
             break
-        case "increment-btn":
+        case e.target.id:
             console.log(e.target.id)
             incrementBtn(e)
             break
@@ -33,7 +30,8 @@ function addHabit(){
    
     let habit = {
         habit: "",
-        counter: 0
+        counter: 0,
+        uuid: uuidv4()
     }
 
     habit.habit = habitEl.value
@@ -41,7 +39,7 @@ function addHabit(){
     if(habit.habit)
     habits.push(habit)
 
-    // console.log(habits)
+    console.log(habits)
 
     renderHabitList()
     habitEl.value = ""
@@ -56,8 +54,8 @@ function renderHabitList(){
         <li class="border habit-item">
             <h3>${habits[i].habit}</h3>
                 <div class="counter">
-                    <p data-num ="increment-btn-${habits[i].habit}">${habits[i].counter}</p>
-                    <button class="increment-btn" id="increment-btn-${habits[i].habit}">+</button>
+                    <p data-num ="${habits[i].uuid}">${habits[i].counter}</p>
+                    <button class="increment-btn" id="${habits[i].uuid}">+</button>
                 </div>
         </li>
         `
@@ -67,5 +65,13 @@ function renderHabitList(){
 
 function incrementBtn(e){
     console.log("registered in increment function " + e.target.id)
-    //document.getElementById(``)
+    const habit = habits.filter(function(habit){
+       return habit.uuid === e.target.id
+    })[0]
+
+    habit.counter ++
+    renderHabitList()
+    //console.log(habits[0].habit + " from habits array in function" )
+
+    //console.log(habit + "found in increment function")
 }
